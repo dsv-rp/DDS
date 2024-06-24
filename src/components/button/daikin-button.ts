ee16524175866815b63b65adb5d2250b7ca1bc08
rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
2024-06-24 16:19:23 +0900

    refactor: implement reviewer suggestion for code reuseable

--- src/components/button/daikin-button.ts
+++ src/components/button/daikin-button.ts
@@ -232,9 +232,6 @@ class DaikinButton extends LitElement implements DaikinButtonProps {
     }
 }
 
-/** Alias of `any` for lit-plugin. We should use `string` for role type because role type can be extended. */
-type AnyRole = any;
-
 declare global {
     interface HTMLElementTagNameMap {
         'daikin-button': DaikinButton;
---
+++ src/lit-workaround-types.ts
@@ -0,0 +1,2 @@
+/** Alias of `any` for lit-plugin. We should use `string` for role type because role type can be extended. */
+type AnyRole = any;
