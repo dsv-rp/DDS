4a6a7289d7e3ae099d29d51b47b32983c81cb78c
rin <106000401+rinjInTokyo@users.noreply.github.com>
2024-06-26 08:56:03 +0900

    refactor: modify a  comment typo
    Co-authored-by: KoharaKazuya <reflect.results@gmail.com>

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,4 @@
-// Ddd this code to improve chrome browser rendering performance when test
+// Add this code to improve chrome browser rendering performance when test
 module.exports = {
   launch: {
     args: [
