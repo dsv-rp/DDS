c4ba03110906e792d6c06b68d5318421068afdfe
yoda-y1 <172582640+yoda-y1@users.noreply.github.com>
2024-06-26 10:41:12 +0900

    test: try updating puppeteer cache

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,25 @@
 // Ddd this code to improve chrome browser rendering performance when test
 module.exports = {
   launch: {
-    product: "firefox",
-    args: ['-wait-for-browser'],
+    headless: "new",
     defaultViewport: { width: 800, height: 600 },
+    args: [
+      "--disable-gpu",
+      "--disable-gpu-compositng",
+      "--disable-accelerated-2d-canvas",
+      "--disable-use-zoom-for-dsf",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-sandbox",
+      "--no-zygote",
+      "--single-process",
+      "--enable-fontations-backend",
+      "--font-render-hinting=none",
+      "--disable-font-subpixel-positioning",
+      "--disable-lcd-text",
+      "--force-color-profile=srgb",
+    ],
   },
   browserContext: "default",
 };
