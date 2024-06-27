6917c055da95b18cc3b2e6cbf307242b3c88ff97
rin <106000401+rinjInTokyo@users.noreply.github.com>
2024-06-27 10:18:35 +0900

    DDS-904 add button component (#2)
    Co-authored-by: lin-l1 <172582502+lin-l1@users.noreply.github.com>
    Co-authored-by: rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
    Co-authored-by: KoharaKazuya <reflect.results@gmail.com>

--- src/components/button/stories/daikin-button-wc.stories.ts
+++ src/components/button/stories/daikin-button-wc.stories.ts
@@ -1,6 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import type { Meta } from '@storybook/web-components';
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 import '../daikin-button';
 import type { DaikinButtonStoryArgs } from './common.ts';
@@ -10,22 +11,40 @@ const meta = {
     tags: ['autodocs'],
     render: (args) => html`
         <daikin-button
-            variant=${args.variant}
+            variant=${ifDefined(args.variant)}
             ?disabled=${args.disabled}
             @click=${action('button-click')}
+            size=${ifDefined(args.size)}
+            href=${ifDefined(args.href)}
+            type=${ifDefined(args.type)}
+            role=${ifDefined(args.role)}
         >
             ${args.label}
         </daikin-button>
-    `,
+        `,
     argTypes: {
         variant: {
             control: { type: 'select' },
-            options: ['primary', 'secondary', 'tertiary', 'primary-danger']
+            options: ['primary', 'secondary', 'tertiary', 'primaryDanger']
         },
         disabled: { type: 'boolean' },
         label: {
             type: 'string'
-        }
+        },
+        size: {
+            control: { type: 'select' },
+            options: ['default', 'condensed']
+        },
+        href: {
+            type: 'string'
+        },
+        type: {
+            control: { type: 'select' },
+            options: ['button' , 'submit' ,'reset']
+        },
+        role: {
+            type: 'string'
+        },
     }
 } satisfies Meta<DaikinButtonStoryArgs>;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @param {import('tailwindcss').Config} config
+ * @returns {import('tailwindcss').Config}
+ */
+function defineConfig(config) {
+  return config
+}
+
+const plugin = require('tailwindcss/plugin');
+
+// @ts-expect-error no type definitions for @daikin-oss/tailwind
 const daikinPlugin = require('@daikin-oss/tailwind');
 
-module.exports = {
+module.exports = defineConfig({
     content: ['./src/**/*.ts|js'],
     theme: {
         extend: {}
     },
-    plugins: [daikinPlugin()]
-};
+    plugins: [
+        daikinPlugin(),
+        plugin(({ addUtilities }) => {
+            addUtilities({
+                '.text-wrap': {
+                    // https://ics.media/entry/240411/
+                    "overflow-wrap": "anywhere",
+                    "word-break": "normal",
+                    "line-break": "strict",
+                },
+            });
+        }),
+    ],
+});
