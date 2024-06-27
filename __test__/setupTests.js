8e3e716daf3cb7595772cb1b127f9118e0401271
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-06-27 11:02:44 +0900

    fix: Threshold Modification

--- __test__/setupTests.js
+++ __test__/setupTests.js
@@ -1,19 +1,19 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
-    failureThreshold: 0.03,
+    failureThreshold: 0.01,
     failureThresholdType: 'percent',
     // I have tried set blur to 1 to solve the problem that there are some noise appear in my snapshot
     // it's better to set blur default
-    blur: 0
+    blur: 0,
 });
 
 expect.extend({ toMatchImageSnapshot });
 
-beforeEach( async() => {
+beforeEach(async () => {
     await page.setViewport({
         width: 800,
         height: 600,
-        deviceScaleFactor: 1
+        deviceScaleFactor: 1,
     });
-})
+});
