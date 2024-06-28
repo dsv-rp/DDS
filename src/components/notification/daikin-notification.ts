5ba61af7ffd4d61df56b559a3070f62127bcc4b9
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-06-28 10:42:42 +0900

    fix: Event names are managed together in a file as constants

--- src/components/notification/daikin-notification.ts
+++ src/components/notification/daikin-notification.ts
@@ -13,6 +13,7 @@ import '../button';
 import tailwindStyles from '../../tailwind.css';
 import styles from './notification.css';
 import { OmitNull } from '../../typeUtils';
+import { EVENT_CLOSE } from '../../constants/events';
 
 const notificationContentsContainerClassName = ctl(`
     flex
@@ -225,7 +226,7 @@ class DaikinNotification extends LitElement implements DaikinNotificationProps {
      * Call the event registered in "close"
      */
     onClickClose() {
-        const event = new CustomEvent('close');
+        const event = new CustomEvent(EVENT_CLOSE);
         this.open = false;
         this.dispatchEvent(event);
     }
