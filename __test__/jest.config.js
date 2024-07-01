75591c9e9e987751f7f82ceccfe162fa176d5a77
rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
2024-07-01 13:58:03 +0900

    Revert "test: ci test error"
    This reverts commit 2eccd46740e8e46ee2b1e3b2ba159e7a611b2e21.

--- __test__/jest.config.js
+++ __test__/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     preset: 'jest-puppeteer',
     testRegex: './*\\.test\\.js$',
-    setupFilesAfterEnv: ['./setupTests.js'],
-    testPathIgnorePatterns: [
-        "./dist",
-      ]
+    setupFilesAfterEnv: ['./setupTests.js']
 };
