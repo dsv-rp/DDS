a5a722cd6ed2d7bc8d8e4dbc7e06d9d4e093fe2d
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-02 16:50:26 +0900

    test: better API for `describeEach`

--- src/tests/visual/runner.ts
+++ src/tests/visual/runner.ts
@@ -2,9 +2,22 @@ import { test } from "@playwright/test";
 
 export function describeEach<T>(
   items: readonly T[],
-  callback: (item: T) => void,
-  titleFormatter: (item: T) => string = String
+  callback: (item: T) => void
+): void;
+export function describeEach<T>(
+  items: readonly T[],
+  titleFormatter: (item: T) => string,
+  callback: (item: T) => void
+): void;
+
+export function describeEach<T>(
+  items: readonly T[],
+  ...args: [(item: T) => void] | [(item: T) => string, (item: T) => void]
 ) {
+  const [callback, titleFormatter] = (
+    args.length === 1 ? [args[0], String] : [args[1], args[0] ?? String]
+  ) as [(item: T) => void, (item: T) => string];
+
   for (const item of items) {
     test.describe(titleFormatter(item), () => callback(item));
   }
