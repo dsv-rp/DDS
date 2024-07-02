8e9f16ec773a5a5e51cf14563c9229e9bea7ca10
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-02 14:05:55 +0900

    chore: inline react component

--- src/components/button/stories/daikin-button-react.stories.tsx
+++ src/components/button/stories/daikin-button-react.stories.tsx
@@ -3,14 +3,12 @@ import type { Meta } from "@storybook/react";
 import { DAIKIN_BUTTON_ARG_TYPES, type DaikinButtonStoryArgs } from "./common";
 import { ReactDaikinButton } from "./react";
 
-const StoryDaikinButton = ({ label, ...props }: DaikinButtonStoryArgs) => {
-  return <ReactDaikinButton {...props}>{label}</ReactDaikinButton>;
-};
-
 export default {
   title: "Components/Button",
   tags: ["autodocs"],
-  component: StoryDaikinButton,
+  component: ({ label, ...props }: DaikinButtonStoryArgs) => (
+    <ReactDaikinButton {...props}>{label}</ReactDaikinButton>
+  ),
   argTypes: DAIKIN_BUTTON_ARG_TYPES,
 } satisfies Meta<DaikinButtonStoryArgs>;
 
