95b1d886b3a7bbcb97f1b82488d67ba1c993e151
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-07-11 17:17:30 +0900

    test: Fixed text displayed in storybook

--- src/components/accordion-group/stories/framework-wc.ts
+++ src/components/accordion-group/stories/framework-wc.ts
@@ -8,13 +8,13 @@ export const metadata: Meta<DaikinAccordionGroupStoryArgs> = {
   render: () => html`
     <daikin-accordion-group
       ><daikin-accordion title="Accordion-1-title"
-        >Accordion-1-body</daikin-accordion
+        >Accordion-1-content</daikin-accordion
       ><daikin-accordion title="Accordion-2-title" open
-        >Accordion-2-body</daikin-accordion
+        >Accordion-2-content</daikin-accordion
       ><daikin-accordion title="Accordion-3-title" disabled
-        >Accordion-3-body</daikin-accordion
+        >Accordion-3-content</daikin-accordion
       ><daikin-accordion title="Accordion-4-title" open disabled
-        >Accordion-4-body</daikin-accordion
+        >Accordion-4-content</daikin-accordion
       ></daikin-accordion-group
     >
   `,
