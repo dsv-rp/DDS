98827d2ccd57add9d2d9809eab4d41068158912b
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-16 10:12:17 +0900

    chore: add comment

--- src/storybook/type-utils.ts
+++ src/storybook/type-utils.ts
@@ -1,7 +1,12 @@
 import type { WebComponentProps } from "@lit/react";
 
-type EventListenerKeys<K> =
-  K extends `on${"ABCDEFGHIJKLMNOPQRSTUVWXYZ"[number]}${string}` ? K : never;
+// A to Z
+type Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[number];
+
+// regex: /^on[A-Z].*$/
+type EventListenerKeys<K> = K extends `on${Uppercase}${string}` ? K : never;
+
+// T without on* props
 type OmitEventListeners<T> = Omit<T, EventListenerKeys<keyof T>>;
 
 export type ElementProps<T extends HTMLElement> = OmitEventListeners<
