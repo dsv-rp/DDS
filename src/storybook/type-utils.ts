8bfe5518e8272ed332e55f15215078801e75d93b
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-16 10:17:41 +0900

    fix: `Uppercase` not working as expected

--- src/storybook/type-utils.ts
+++ src/storybook/type-utils.ts
@@ -1,12 +1,37 @@
 import type { WebComponentProps } from "@lit/react";
 
-// A to Z
-type Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[number];
+type Uppercase =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
 
-// regex: /^on[A-Z].*$/
+// regex: /^on[A-Z]/
 type EventListenerKeys<K> = K extends `on${Uppercase}${string}` ? K : never;
 
-// T without on* props
+// T without /^on[A-Z]/ props
 type OmitEventListeners<T> = Omit<T, EventListenerKeys<keyof T>>;
 
 export type ElementProps<T extends HTMLElement> = OmitEventListeners<
