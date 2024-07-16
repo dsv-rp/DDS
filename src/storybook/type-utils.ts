b75ce54cfc95312cc65475c115596d946231c5a6
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-16 15:04:20 +0900

    chore: remove unnecessary `${string}`

--- src/storybook/type-utils.ts
+++ src/storybook/type-utils.ts
@@ -2,9 +2,7 @@ import type { WebComponentProps } from "@lit/react";
 
 // regex: /^on[A-Z]/
 // Note that `Capitalize<string>` includes strings that contain uppercase letters from the second character onwards. (e.g. `KeyDown`)
-type EventListenerKeys<K> = K extends `on${Capitalize<string>}${string}`
-  ? K
-  : never;
+type EventListenerKeys<K> = K extends `on${Capitalize<string>}` ? K : never;
 
 // T without /^on[A-Z]/ props
 type OmitEventListeners<T> = Omit<T, EventListenerKeys<keyof T>>;
