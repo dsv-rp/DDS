2901b81e9eb6e28661e3f0c6f850b4d11d62ffb4
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-16 14:31:09 +0900

    chore: simplify types

--- src/storybook/type-utils.ts
+++ src/storybook/type-utils.ts
@@ -1,35 +1,10 @@
 import type { WebComponentProps } from "@lit/react";
 
-type Uppercase =
-  | "A"
-  | "B"
-  | "C"
-  | "D"
-  | "E"
-  | "F"
-  | "G"
-  | "H"
-  | "I"
-  | "J"
-  | "K"
-  | "L"
-  | "M"
-  | "N"
-  | "O"
-  | "P"
-  | "Q"
-  | "R"
-  | "S"
-  | "T"
-  | "U"
-  | "V"
-  | "W"
-  | "X"
-  | "Y"
-  | "Z";
-
 // regex: /^on[A-Z]/
-type EventListenerKeys<K> = K extends `on${Uppercase}${string}` ? K : never;
+// Note that `Capitalize<string>` includes strings that contain uppercase letters from the second character onwards. (e.g. `KeyDown`)
+type EventListenerKeys<K> = K extends `on${Capitalize<string>}${string}`
+  ? K
+  : never;
 
 // T without /^on[A-Z]/ props
 type OmitEventListeners<T> = Omit<T, EventListenerKeys<keyof T>>;
