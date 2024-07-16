08120ed543227f9c21b97165cef42bb7af461625
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-07-16 15:26:14 +0900

    Fix react interaction test not working at all (#23)

--- src/storybook/type-utils.ts
+++ src/storybook/type-utils.ts
@@ -1,3 +1,12 @@
 import type { WebComponentProps } from "@lit/react";
 
-export type ElementProps<T extends HTMLElement> = WebComponentProps<T>;
+// regex: /^on[A-Z]/
+// Note that `Capitalize<string>` includes strings that contain uppercase letters from the second character onwards. (e.g. `KeyDown`)
+type EventListenerKeys<K> = K extends `on${Capitalize<string>}` ? K : never;
+
+// T without /^on[A-Z]/ props
+type OmitEventListeners<T> = Omit<T, EventListenerKeys<keyof T>>;
+
+export type ElementProps<T extends HTMLElement> = OmitEventListeners<
+  WebComponentProps<T>
+>;
