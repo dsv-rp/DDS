6b0ec5630bb93c04fa029813d1f16ac26ad93bf4
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-07-22 17:47:42 +0900

    test: Update test code

--- src/components/dropdown-item/daikin-dropdown-item.visual.test.ts
+++ src/components/dropdown-item/daikin-dropdown-item.visual.test.ts
@@ -40,3 +40,36 @@ test("hover", async ({ page }) => {
   // take screenshot and check for diffs
   await expect(page).toHaveScreenshot(await clipFor(element));
 });
+
+test("active", async ({ page }) => {
+  await page.goto(baseURL);
+
+  // wait for element to be visible
+  const element = await page.waitForSelector("daikin-dropdown-item", {
+    state: "visible",
+  });
+
+  // hover cursor on the element
+  await element.hover();
+  await page.mouse.down();
+
+  // take screenshot and check for diffs
+  await expect(page).toHaveScreenshot(await clipFor(element));
+  await page.mouse.up();
+});
+
+test("focus", async ({ page }) => {
+  await page.goto(baseURL);
+
+  // wait for element to be visible
+  const element = await page.waitForSelector("daikin-dropdown-item", {
+    state: "visible",
+  });
+
+  await page.evaluate((container) => {
+    container.focus();
+  }, element);
+
+  // take screenshot and check for diffs
+  await expect(page).toHaveScreenshot(await clipFor(element));
+});
--- src/components/dropdown/daikin-dropdown.visual.test.ts
+++ src/components/dropdown/daikin-dropdown.visual.test.ts
@@ -13,34 +13,100 @@ const getPageURL = (args: StoryArgs = {}) =>
   getStorybookIframeURL("components-dropdown--default", args);
 
 describeEach(["small", "medium"] as const, (size) => {
-  describeEach(["exist", "none"] as const, (label) => {
-    describeEach(["top", "left"] as const, (labelPosition) => {
-      describeEach(["exist", "none"] as const, (leftIcon) => {
-        describeEach(["open", "close"] as const, (state) => {
-          const baseURL = getPageURL({
-            size,
-            label: label === "exist" ? "Dropdown Label" : "",
-            labelPosition,
-            ...(state === "open" && { open: true }),
-            ...(leftIcon === "exist" && { leftIcon: "positive" as const }),
-          });
+  describeEach(["top", "left", "hidden"] as const, (labelPosition) => {
+    describeEach(["left", "none"] as const, (icon) => {
+      describeEach(["open", "close"] as const, (state) => {
+        const baseURL = getPageURL({
+          size,
+          label: "Dropdown Label",
+          labelPosition,
+          ...(state === "open" && { open: true }),
+          ...(icon === "left" && { leftIcon: "positive" as const }),
+        });
 
-          test("base", async ({ page }) => {
-            await page.goto(baseURL);
+        test("base", async ({ page }) => {
+          await page.goto(baseURL);
 
-            // wait for element to be visible
-            const element = await page.waitForSelector(
-              `div[data-testid="vrt-container"]`,
-              {
-                state: "visible",
-              }
-            );
+          // wait for element to be visible
+          const element = await page.waitForSelector(
+            `div[data-testid="vrt-container"]`,
+            {
+              state: "visible",
+            }
+          );
 
-            // take screenshot and check for diffs
-            await expect(page).toHaveScreenshot(await clipFor(element));
-          });
+          // take screenshot and check for diffs
+          await expect(page).toHaveScreenshot(await clipFor(element));
         });
       });
     });
   });
+});
+
+test("hover", async ({ page }) => {
+  const baseURL = getPageURL();
+  await page.goto(baseURL);
+  // wait for element to be visible
+  const element = await page.waitForSelector(
+    `div[data-testid="vrt-container"]`,
+    {
+      state: "visible",
+    }
+  );
+  const button = await element.waitForSelector("button", {
+    state: "visible",
+  });
+
+  // hover cursor on the element
+  await button.hover();
+
+  // take screenshot and check for diffs
+  await expect(page).toHaveScreenshot(await clipFor(element));
+});
+
+test("active", async ({ page }) => {
+  const baseURL = getPageURL();
+  await page.goto(baseURL);
+
+  // wait for element to be visible
+  const element = await page.waitForSelector(
+    `div[data-testid="vrt-container"]`,
+    {
+      state: "visible",
+    }
+  );
+  const button = await element.waitForSelector("button", {
+    state: "visible",
+  });
+
+  // hover cursor on the element
+  await button.hover();
+  await page.mouse.down();
+
+  // take screenshot and check for diffs
+  await expect(page).toHaveScreenshot(await clipFor(element));
+  await page.mouse.up();
+});
+
+test("focus", async ({ page }) => {
+  const baseURL = getPageURL();
+  await page.goto(baseURL);
+
+  // wait for element to be visible
+  const element = await page.waitForSelector(
+    `div[data-testid="vrt-container"]`,
+    {
+      state: "visible",
+    }
+  );
+  const button = await element.waitForSelector("button", {
+    state: "visible",
+  });
+
+  await page.evaluate((container) => {
+    container.focus();
+  }, button);
+
+  // take screenshot and check for diffs
+  await expect(page).toHaveScreenshot(await clipFor(element));
 });
