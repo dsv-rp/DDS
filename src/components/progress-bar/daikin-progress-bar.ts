95d6a865725d7c828f2215070c5c37ceb07f8524
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-08-05 17:07:30 +0900

    fix: Correct the problems pointed out

--- src/components/progress-bar/daikin-progress-bar.ts
+++ src/components/progress-bar/daikin-progress-bar.ts
@@ -11,23 +11,23 @@ const cvaBar = cva(["w-full", "h-1", "overflow-hidden"], {
   variants: {
     variant: {
       inprogress: [
-        "bg-[#DCDCDC]",
+        "bg-daikinNeutral-200",
         "relative",
         "after:block",
         "after:w-[--bar-width]",
         "after:h-full",
-        "after:bg-[#0097e0]",
+        "after:bg-daikinBlue-500",
         "after:absolute",
         "after:left-0",
       ],
       completed: ["bg-[--colorFeedbackPositive]"],
       indeterminate: [
-        "bg-[#DCDCDC]",
+        "bg-daikinNeutral-200",
         "relative",
         "after:block",
         "after:w-1/2",
         "after:h-full",
-        "after:bg-[#0097e0]",
+        "after:bg-daikinBlue-500",
         "after:absolute",
         "after:left-0",
         "after:animate-[progress-bar-indeterminate_1200ms_linear_infinite]",
@@ -62,7 +62,12 @@ const cvaHelper = cva(["text-xs", "mt-2"], {
 /**
  * The progress bar component is used to visually convey the progress to the user.
  *
- * When showing that something is in progress, you can choose between a mode that gives a percentage and illustrates accurate information, and a mode that continues to animate as an indefinite value.
+ * There are four variants of the progress bar:
+ *
+ * - "inprogress": The default variant. Use this when you know the exact progress. The length of the bar is the ratio of `value` to `max`.
+ * - "indeterminate": Use this variant when you don't know the exact progress. In this variant, the `value` is ignored and the bar always animates from left to right with a constant length.
+ * - "completed": In addition to "inprogress", a completed icon is added. The bar length refers to the `value`, so the user needs to update the `value` so that it is the same as `max`.
+ * - "error": In addition to "inprogress", an error icon is added and the color becomes red. The length of the bar refers to the `value`. When transitioning from "indeterminate", you need to set an appropriate (but not need to be accurate) `value`.
  *
  * @example
  *
