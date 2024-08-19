5ec1e6ba752063cac842e81c13bf554feccd3bf3
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-08-19 11:35:20 +0900

    test: Add default table snapshot

--- src/components/table/daikin-table.visual.test.ts
+++ src/components/table/daikin-table.visual.test.ts
@@ -1,17 +1,20 @@
 import { clipFor, describeEach, getStorybookIframeURL } from "#tests/visual";
 import { expect, test } from "@playwright/test";
 
-describeEach(["checkbox", "search", "sort", "pagination", "all"], (option) => {
-  test("base", async ({ page }) => {
-    const baseURL = getStorybookIframeURL(`components-table--${option}`, {});
-    await page.goto(baseURL);
+describeEach(
+  ["default", "checkbox", "search", "sort", "pagination", "all"],
+  (option) => {
+    test("base", async ({ page }) => {
+      const baseURL = getStorybookIframeURL(`components-table--${option}`, {});
+      await page.goto(baseURL);
 
-    // wait for element to be visible
-    const element = await page.waitForSelector("daikin-table", {
-      state: "visible",
+      // wait for element to be visible
+      const element = await page.waitForSelector("daikin-table", {
+        state: "visible",
+      });
+
+      // take screenshot and check for diffs
+      await expect(page).toHaveScreenshot(await clipFor(element));
     });
-
-    // take screenshot and check for diffs
-    await expect(page).toHaveScreenshot(await clipFor(element));
-  });
-});
+  }
+);
