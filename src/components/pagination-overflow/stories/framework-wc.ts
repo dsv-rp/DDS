3d7bfc3dd97837ca2d61767eeea237bf6d131f6b
rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
2024-08-21 17:58:23 +0900

    fix: wc interaction test error

--- src/components/pagination-overflow/stories/framework-wc.ts
+++ src/components/pagination-overflow/stories/framework-wc.ts
@@ -4,12 +4,13 @@ import { html } from "lit";
 import type { DaikinPaginationOverflowStoryArgs } from "./common";
 
 export const metadata: Meta<DaikinPaginationOverflowStoryArgs> = {
-  render: ({ value, max, totalItems }) => html`
+  render: ({ value, max, totalItems, onChange }) => html`
     <div id="storyWrap" style="width:400px; height:200px">
       <daikin-pagination-overflow
         value=${value}
         max=${max}
         total-items=${totalItems}
+        @page-change=${onChange}
       >
       </daikin-pagination-overflow>
     </div>
