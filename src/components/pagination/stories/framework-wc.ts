04893162c39f5e157f440183403e280ff3ffab30
rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
2024-08-26 09:30:39 +0900

    fix: wc interaction error

--- src/components/pagination/stories/framework-wc.ts
+++ src/components/pagination/stories/framework-wc.ts
@@ -4,12 +4,14 @@ import { html } from "lit";
 import type { DaikinPaginationStoryArgs } from "./common";
 
 export const metadata: Meta<DaikinPaginationStoryArgs> = {
-  render: ({ currentPage, lastPage, pageWindow }) => html`
+  render: ({ currentPage, lastPage, pageWindow, onChange, onClick }) => html`
     <div id="storyWrap" style="width: 600px; height: 300px">
       <daikin-pagination
         current-page=${currentPage}
         last-page=${lastPage}
         page-window=${pageWindow}
+        @page-change=${onChange}
+        @click=${onClick}
       >
       </daikin-pagination>
     </div>
