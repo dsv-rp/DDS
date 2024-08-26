3c0ee6be12cc8c99739e98190f46236e15cf2d4f
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-08-26 11:43:33 +0900

    test: Simplified test items

--- src/components/notification/daikin-notification.visual.test.ts
+++ src/components/notification/daikin-notification.visual.test.ts
@@ -52,31 +52,22 @@ describeEach(["toast", "inline"] as const, (variant) => {
 });
 
 describeEach(["attribute", "slot"] as const, (insert) => {
-  describeEach(["title", "description"] as const, (text) => {
-    const baseURL = getPageURL({
-      variant: "toast",
-      title: "Example Notification by attribute",
-      description: "Description of the notification by attribute",
-      ...(insert === "slot" &&
-        text === "title" && {
-          __vrtTitle__: "Example Notification by slot",
-        }),
-      ...(insert === "slot" &&
-        text === "description" && {
-          __vrtDescription__: "Description of the notification by slot",
-        }),
+  const baseURL = getPageURL({
+    variant: "toast",
+    title: "Example Notification by attribute",
+    description: "Description of the notification by attribute",
+    ...(insert === "slot" && {
+      __vrtTitle__: "Example Notification by slot",
+      __vrtDescription__: "Description of the notification by slot",
+    }),
+  });
+  test("base", async ({ page }) => {
+    await page.goto(baseURL);
+    // wait for element to be visible
+    const element = await page.waitForSelector("daikin-notification", {
+      state: "visible",
     });
-
-    test("base", async ({ page }) => {
-      await page.goto(baseURL);
-
-      // wait for element to be visible
-      const element = await page.waitForSelector("daikin-notification", {
-        state: "visible",
-      });
-
-      // take screenshot and check for diffs
-      await expect(page).toHaveScreenshot(await clipFor(element));
-    });
+    // take screenshot and check for diffs
+    await expect(page).toHaveScreenshot(await clipFor(element));
   });
 });
