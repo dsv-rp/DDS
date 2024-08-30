c9b419b6cb9b9dd52f0438bec3d1838215871a54
rinjInTokyo <1060000401+rinjInTokyo@users.noreply.github.com>
2024-08-30 17:17:44 +0900

    refactor: delete useless code

--- src/components/pagination/pagination-utils.ts
+++ src/components/pagination/pagination-utils.ts
@@ -10,7 +10,6 @@ export function calculatePagination(
   currentPage: number,
   pageWindow: number
 ): PaginationContent {
-  // ...
   const result = {
     leftMost: 1,
     leftEllipsis: [],
@@ -22,10 +21,9 @@ export function calculatePagination(
     pageWindow = 5;
   }
   if (lastPage <= pageWindow) {
-    result.middle = range(2, lastPage, 1);
+    result.middle = range(2, lastPage);
     return result;
   }
-  const adjustFlag = (pageWindow - 3) % 2 === 0 ? 1 : 0;
   if (currentPage < pageWindow - 2) {
     result.middle = range(2, pageWindow - 1);
     result.rightEllipsis = range(pageWindow - 1, lastPage);
@@ -36,11 +34,10 @@ export function calculatePagination(
     result.middle = range(lastPage - (pageWindow - 3), lastPage);
     return result;
   }
-  const leftPages = Math.max(2, currentPage - Math.floor((pageWindow - 3) / 2));
-  const rightPages = Math.min(
-    lastPage - 1,
-    currentPage + Math.floor((pageWindow - 3) / 2)
-  );
+
+  const adjustFlag = (pageWindow - 3) % 2 === 0 ? 1 : 0;
+  const leftPages = currentPage - Math.floor((pageWindow - 3) / 2);
+  const rightPages = currentPage + Math.floor((pageWindow - 3) / 2);
   result.leftEllipsis = range(2, leftPages + adjustFlag);
   result.rightEllipsis = range(rightPages, lastPage);
   result.middle = range(leftPages + adjustFlag, rightPages);
