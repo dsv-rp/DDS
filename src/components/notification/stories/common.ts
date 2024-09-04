fdcb4b9bf604630cbb3cb09e772b5358ead8296c
poetrainy <75821494+poetrainy@users.noreply.github.com>
2024-09-04 10:15:38 +0900

    fix: Update Storybook args type

--- src/components/notification/stories/common.ts
+++ src/components/notification/stories/common.ts
@@ -4,42 +4,35 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 
 export interface DaikinNotificationStoryArgs
   extends Required<ElementProps<DaikinNotification>> {
-  slotTitle?: string;
-  slotDescription?: string;
+  slotTitle: string;
+  slotDescription: string;
   onClose: () => void;
 }
 
 export const DAIKIN_NOTIFICATION_ARG_TYPES = {
   variant: {
-    defaultValue: "toast",
-    control: { type: "radio" },
+    control: "radio",
     options: ["toast", "inline"],
   },
   status: {
-    defaultValue: "positive",
-    control: { type: "radio" },
+    control: "radio",
     options: ["positive", "negative", "warning", "alarm", "information"],
   },
   line: {
-    defaultValue: "single",
-    control: { type: "radio" },
+    control: "radio",
     options: ["single", "multi"],
   },
   open: {
-    defaultValue: true,
     type: "boolean",
   },
   closeButton: {
-    defaultValue: false,
     type: "boolean",
   },
   slotTitle: {
     type: "string",
-    defaultValue: "Notification title",
   },
   slotDescription: {
     type: "string",
-    defaultValue: "Notification description",
   },
 } satisfies Meta<DaikinNotificationStoryArgs>["argTypes"];
 
