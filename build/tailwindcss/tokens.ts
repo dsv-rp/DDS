ac1390ecc251ca7e44cc25d22d04dec55d1d9195
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-10-04 10:28:09 +0900

    fix: support missing token types

--- build/tailwindcss/tokens.ts
+++ build/tailwindcss/tokens.ts
@@ -15,14 +15,14 @@ type TokenType =
 function createPerTypeTokenMap(
   keyPrefix: string,
   cssVarPrefix: string
-): Record<TokenType, Record<string, string>> {
+): Partial<Record<TokenType, Record<string, string>>> {
   return Object.entries(designTokens).reduce<
     Partial<Record<TokenType, Partial<Record<string, string>>>>
   >((acc, [key, [value, type]]) => {
     const object = (acc[type as TokenType] ??= {});
     object[`${keyPrefix}${key}`] = `var(${cssVarPrefix}${key}) /* ${value} */`;
     return acc;
-  }, {}) as Record<TokenType, Record<string, string>>;
+  }, {}) as Partial<Record<TokenType, Record<string, string>>>;
 }
 
 export const tokensPlugin = plugin.withOptions<{
@@ -35,7 +35,7 @@ export const tokensPlugin = plugin.withOptions<{
 
       // Add `b-ddt-*` and `o-ddt-*`.
       addUtilities(
-        Object.entries(perTypeTokenMap.border).map(
+        Object.entries(perTypeTokenMap.border ?? {}).map(
           ([key, value]): Record<string, Record<string, string>> => ({
             [`.b-${key}`]: {
               border: value,
@@ -69,7 +69,7 @@ export const tokensPlugin = plugin.withOptions<{
 
       // Add `type-ddt-*`.
       addUtilities(
-        Object.entries(perTypeTokenMap.typography).map(
+        Object.entries(perTypeTokenMap.typography ?? {}).map(
           ([key, value]): Record<string, Record<string, string>> => ({
             [`.type-${key}`]: {
               font: value,
