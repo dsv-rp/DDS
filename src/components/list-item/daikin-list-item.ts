6f3a963276f3324e162224a27cd660a8e9d4a852
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-10-11 11:52:38 +0900

    fix: fix lint error

--- src/components/list-item/daikin-list-item.ts
+++ src/components/list-item/daikin-list-item.ts
@@ -130,7 +130,7 @@ export class DaikinListItem extends LitElement {
     }[this.type]();
 
     return html`<div
-      class="flex justify-between items-center w-full min-h-12 p-3 text-left relative [&:has(a:not(:any-link),:disabled)]:text-daikinNeutral-200"
+      class=${"flex justify-between items-center w-full min-h-12 p-3 text-left relative [&:has(a:not(:any-link),:disabled)]:text-daikinNeutral-200"}
       role="listitem"
     >
       ${list}
