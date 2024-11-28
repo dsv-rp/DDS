651bc767f92be4a5b48b4c8c48b3378ef0e13855
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-11-28 10:22:58 +0900

    DDS-1355 DDS-1429 Add ESLint rules, Rename/remove some Attributes (#53)

---
+++ .changeset/soft-tomatoes-double.md
@@ -0,0 +1,9 @@
+---
+"@daikin-oss/design-system-web-components": minor
+---
+
+**BREAKING CHANGE** Some attributes and properties have been renamed or removed.
+
+The following attributes (not properties) have been renamed to kebab case:
+
+- `<daikin-notification>`: ~~`closeButton`~~ -> `close-button`
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import eslint from "@eslint/js";
-import prettierPkg from "eslint-config-prettier";
+import prettierConfig from "eslint-config-prettier";
 import { configs as litConfig } from "eslint-plugin-lit";
 import {
   configs as litA11yConfigLegacy,
@@ -7,8 +7,6 @@ import {
 } from "eslint-plugin-lit-a11y";
 import { configs as wcConfig } from "eslint-plugin-wc";
 import tseslint from "typescript-eslint";
-
-const { rules: prettierRules } = prettierPkg;
 
 // convert eslint-plugin-lit-a11y's recommended config into a flat one
 const litA11yConfigFlatRecommended = {
@@ -26,7 +24,22 @@ export default tseslint.config(
   wcConfig["flat/best-practice"],
   litConfig["flat/recommended"],
   litA11yConfigFlatRecommended,
-  { rules: prettierRules },
+  prettierConfig,
+  // Ignore files
+  {
+    ignores: ["dist", "storybook-static", "test-results"],
+  },
+  // CJS
+  {
+    files: ["**/*.cjs", "**/*.cts"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  // Base rules
   {
     languageOptions: {
       parserOptions: {
@@ -50,6 +63,18 @@ export default tseslint.config(
           allowNumber: true,
         },
       ],
+
+      // Web Components and lit
+      "lit/attribute-names": "error",
+      "lit/lifecycle-super": "error",
+      "lit/no-classfield-shadowing": "error",
+      "lit/no-native-attributes": "error",
+      "lit/no-this-assign-in-render": "error",
+      "lit/prefer-nothing": "warn",
+      "lit/prefer-static-styles": "error",
+      "lit/quoted-expressions": "error",
+      "lit/value-after-constraints": "error",
+
       "wc/guard-super-call": "off", // Disabled as it's checked by TypeScript
       "lit-a11y/anchor-is-valid": [
         "error",
@@ -60,16 +85,62 @@ export default tseslint.config(
       ],
     },
   },
+  // Restrict imports
   {
-    files: ["**/*.cjs", "**/*.cts"],
-    languageOptions: {
-      sourceType: "commonjs",
-    },
+    // Storybook files
+    files: ["src/components/*/stories/*.{ts,tsx}"],
     rules: {
-      "@typescript-eslint/no-require-imports": "off",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            // Restrict importing `../example-component`.
+            // In order to test the built components, we have to use `#package/` to reference them.
+            {
+              regex: "^\\.\\./[\\w-]+",
+              message: "Use #package/ to import components.",
+            },
+            // Restrict importing `../../example-component/*`, allowing `../../example-component/stories/*`.
+            {
+              regex: "^\\.\\./\\.\\./[\\w-]+/(?!stories/)[\\w-]+",
+              message: "Use #package/ to import components.",
+            },
+            // Restrict importing `#package/components/example-component/stories/*`.
+            // Storybook files are not built as a library, so they cannot be referenced with `#package/`.
+            {
+              regex: "^#package/components/[\\w-]+/stories/",
+              message: "Use ../../*/stories/* to import other Storybook files.",
+            },
+          ],
+        },
+      ],
     },
   },
   {
-    ignores: ["dist", "storybook-static", "test-results"],
+    // Component files, excluding Storybook files and test files
+    files: ["src/components/*.ts"],
+    ignores: ["*.visual.test.ts"],
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            // The component code itself cannot use `#package/`.
+            {
+              regex: "^#package/",
+              message: "Do not use #package/ in component files.",
+            },
+            {
+              regex: "/stories/|^#storybook/",
+              message: "Do not import Storybook files.",
+            },
+            {
+              regex: "\\.test(\\.[\\w-]+)?$|^#tests/|/tests/",
+              message: "Do not import test files.",
+            },
+          ],
+        },
+      ],
+    },
   }
 );
