598656fc3851ac15461ffff9ca228fa9ad904deb
rin <106000401+rinjInTokyo@users.noreply.github.com>
2024-12-24 17:12:18 +0900

    DDS-1209 [Card] Add Card Component (#111)
    Co-authored-by: yodas7 <173234870+yodas7@users.noreply.github.com>

---
+++ src/components/card-header/daikin-card-header.visual.test.ts
@@ -0,0 +1,40 @@
+import {
+  clipFor,
+  describeEach,
+  getStorybookIframeURL,
+  type InferStorybookArgTypes,
+} from "#tests/visual";
+import { expect, test } from "@playwright/test";
+import type { DAIKIN_CARD_HEADER_ARG_TYPES } from "./stories/common";
+
+type StoryArgs = InferStorybookArgTypes<typeof DAIKIN_CARD_HEADER_ARG_TYPES>;
+
+const getPageURL = (args: StoryArgs = {}) =>
+  getStorybookIframeURL("components-card-header--default", args);
+
+describeEach(["light", "dark"] as const, (theme) => {
+  describeEach(["icon", "description", "both"], (structure) => {
+    const baseArgs = {
+      $theme: theme,
+      label: "Card Title",
+      description:
+        structure === "description" || structure === "both"
+          ? "Description"
+          : "",
+      leftIcon: structure === "icon" || structure === "both" ? "alarm" : "",
+    };
+    const baseURL = getPageURL(baseArgs);
+
+    test("base", async ({ page }) => {
+      await page.goto(baseURL);
+
+      // wait for element to be visible
+      const element = await page.waitForSelector("daikin-card-header", {
+        state: "visible",
+      });
+
+      // take screenshot and check for diffs
+      await expect(page).toHaveScreenshot(await clipFor(element));
+    });
+  });
+});
