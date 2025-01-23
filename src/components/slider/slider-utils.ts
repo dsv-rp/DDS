9852962843a048db6070dad76df16846c34f7bf1
rinjInTokyo <106000401+rinjInTokyo@users.noreply.github.com>
2025-01-23 16:36:42 +0900

    refactor: refactor by reviewer's suggestion

--- src/components/slider/slider-utils.ts
+++ src/components/slider/slider-utils.ts
@@ -1,30 +1,17 @@
 import type { DaikinSlider } from "./daikin-slider";
 
 /**
- * Change the slider properties from string to float for calculate.
+ * This function is used to handle floating-point errors in calculations.
+ * Ensuring that the number of decimal places matches the decimal places of the step value.
  *
- * @param min The min value of the slider.
- * @param max The max value of the slider.
- * @param step The step of the slider.
- * @param value The current value of the slider.
- * @returns An array of parsed float value.
+ * @param value The current value.
+ * @param step The step value.
+ * @returns Formatted current value.
  */
-export function parsedToFloat(
-  min: string,
-  max: string,
-  step: string,
-  value: string
-): number[] {
-  const minFloat = parseFloat(min);
-  const maxFloat = parseFloat(max);
-  const stepFloat = parseFloat(step);
-  const valueFloat = parseFloat(value);
-  return [
-    minFloat,
-    maxFloat,
-    stepFloat,
-    Math.max(minFloat, Math.min(valueFloat, maxFloat)),
-  ];
+export function formatValue(value: number, step: string) {
+  const decimals = step.includes(".") ? step.split(".")[1].length : 0;
+  const clampedValue = parseFloat(value.toFixed(decimals));
+  return `${clampedValue}`;
 }
 
 /**
@@ -35,23 +22,13 @@ export function parsedToFloat(
  * @returns Current value.
  */
 export function getValueFromRatio(slider: DaikinSlider, ratio: number): string {
-  const [minFloat, maxFloat, stepFloat] = parsedToFloat(
-    slider.min,
-    slider.max,
-    slider.step,
-    slider.value
-  );
-  const decimals = slider.step.includes(".")
-    ? slider.step.split(".")[1].length
-    : 0;
-
+  const minFloat = parseFloat(slider.min);
+  const maxFloat = parseFloat(slider.max);
+  const stepFloat = parseFloat(slider.step);
   const rawValue = ratio * (maxFloat - minFloat) + minFloat;
   const steppedValue = Math.round(rawValue / stepFloat) * stepFloat;
-  const clampedValue = Math.max(
-    minFloat,
-    Math.min(maxFloat, parseFloat(steppedValue.toFixed(decimals)))
-  );
-  return `${clampedValue}`;
+  const clampedValue = Math.max(minFloat, Math.min(maxFloat, steppedValue));
+  return formatValue(clampedValue, slider.step);
 }
 
 /**
@@ -71,29 +48,22 @@ export function getValueByKeydown(
   if (key === "End") {
     return slider.max;
   }
-  const step = parseFloat(slider.step);
-  const moveOffset = {
-    ArrowRight: step,
-    ArrowDown: -step,
-    ArrowLeft: -step,
-    ArrowUp: step,
+  const offset = {
+    ArrowLeft: -1,
+    ArrowDown: -1,
+    ArrowRight: 1,
+    ArrowUp: 1,
   }[key];
-  if (!moveOffset) {
+  if (!offset) {
     return;
   }
-  const [minFloat, maxFloat, , valueFloat] = parsedToFloat(
-    slider.min,
-    slider.max,
-    slider.step,
-    slider.value
-  );
-  const decimals = slider.step.includes(".")
-    ? slider.step.split(".")[1].length
-    : 0;
-
+  const minFloat = parseFloat(slider.min);
+  const maxFloat = parseFloat(slider.max);
+  const stepFloat = parseFloat(slider.step);
+  const valueFloat = parseFloat(slider.value);
   const clampedValue = Math.max(
     minFloat,
-    Math.min(maxFloat, parseFloat((valueFloat + moveOffset).toFixed(decimals)))
+    Math.min(maxFloat, valueFloat + stepFloat * offset)
   );
-  return `${clampedValue}`;
+  return formatValue(clampedValue, slider.step);
 }
--- src/components/slider/slider-utils.unit.test.ts
+++ src/components/slider/slider-utils.unit.test.ts
@@ -1,52 +1,6 @@
 import { describe, it } from "vitest";
 import { DaikinSlider } from "./daikin-slider";
-import {
-  getValueByKeydown,
-  getValueFromRatio,
-  parsedToFloat,
-} from "./slider-utils";
-
-describe("test parsedToFloat function", () => {
-  it("min: '1'; max: '100'; step: '1'; value: '50'", ({ expect }) => {
-    const [min, max, step, value] = parsedToFloat("1", "100", "2", "50");
-    expect(min).toEqual(1);
-    expect(max).toEqual(100);
-    expect(step).toEqual(2);
-    expect(value).toEqual(50);
-  });
-
-  it("min: '1'; max: '100'; step: '0.01'; value: '50'", ({ expect }) => {
-    const [min, max, step, value] = parsedToFloat("1", "100", "0.01", "50");
-    expect(min).toEqual(1);
-    expect(max).toEqual(100);
-    expect(step).toEqual(0.01);
-    expect(value).toEqual(50);
-  });
-
-  it("min: '1'; max: '100'; step: '0.01000'; value: '50'", ({ expect }) => {
-    const [min, max, step, value] = parsedToFloat("1", "100", "0.01", "50");
-    expect(min).toEqual(1);
-    expect(max).toEqual(100);
-    expect(step).toEqual(0.01);
-    expect(value).toEqual(50);
-  });
-
-  it("min: '1'; max: '100'; step: '0.010001'; value: '50'", ({ expect }) => {
-    const [min, max, step, value] = parsedToFloat("1", "100", "0.010001", "50");
-    expect(min).toEqual(1);
-    expect(max).toEqual(100);
-    expect(step).toEqual(0.010001);
-    expect(value).toEqual(50);
-  });
-
-  it("min: '1'; max: '100'; step: '1'; value: '200'", ({ expect }) => {
-    const [min, max, step, value] = parsedToFloat("1", "100", "2", "200");
-    expect(min).toEqual(1);
-    expect(max).toEqual(100);
-    expect(step).toEqual(2);
-    expect(value).toEqual(100);
-  });
-});
+import { getValueByKeydown, getValueFromRatio } from "./slider-utils";
 
 describe("test getValueFromRatio function", () => {
   it("min: '0'; max: '100'; step: '1'; leftDistance: 0.5", ({ expect }) => {
@@ -111,6 +65,28 @@ describe("test getValueFromRatio function", () => {
     const value = getValueFromRatio(slider, 0.21);
     expect(value).toEqual("2.1");
   });
+
+  it("min: '0'; max: '10'; step: '0.01000'; leftDistance: 0.5", ({
+    expect,
+  }) => {
+    const slider = new DaikinSlider();
+    slider.min = "0";
+    slider.max = "10";
+    slider.step = "0.01000";
+    const value = getValueFromRatio(slider, 0.5);
+    expect(value).toEqual("5");
+  });
+
+  it("min: '0'; max: '10'; step: '0.01001'; leftDistance: 0.5", ({
+    expect,
+  }) => {
+    const slider = new DaikinSlider();
+    slider.min = "0";
+    slider.max = "10";
+    slider.step = "0.01001";
+    const value = getValueFromRatio(slider, 0.5);
+    expect(value).toEqual("5.005");
+  });
 });
 
 describe("test getValueByKeydown function", () => {
@@ -167,5 +143,27 @@ describe("test getValueByKeydown function", () => {
 
     const value = getValueByKeydown(slider, "ArrowRight");
     expect(value).toEqual("9");
+  });
+
+  it("min: '1'; max: '10'; step: '0.01000'; value: '8'", ({ expect }) => {
+    const slider = new DaikinSlider();
+    slider.min = "1";
+    slider.max = "10";
+    slider.value = "8";
+    slider.step = "0.01000";
+
+    const value = getValueByKeydown(slider, "ArrowRight");
+    expect(value).toEqual("8.01");
+  });
+
+  it("min: '1'; max: '10'; step: '0.01001'; value: '8'", ({ expect }) => {
+    const slider = new DaikinSlider();
+    slider.min = "1";
+    slider.max = "10";
+    slider.value = "8";
+    slider.step = "0.01001";
+
+    const value = getValueByKeydown(slider, "ArrowRight");
+    expect(value).toEqual("8.01001");
   });
 });
