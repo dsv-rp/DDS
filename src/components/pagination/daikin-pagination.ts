05560a1d6fd61a3aed27e4b815d7207f031bc466
rinjInTokyo <106000401+rinjInTokyo@users.noreply.github.com>
2025-02-07 11:12:55 +0900

    fix: pagination-002 change pagination line

--- src/components/pagination/daikin-pagination.ts
+++ src/components/pagination/daikin-pagination.ts
@@ -171,7 +171,11 @@ export class DaikinPagination extends LitElement {
       this.window
     );
     return html`
-      <div aria-label="Pagination" role="navigation" class="inline-flex">
+      <div
+        aria-label="Pagination"
+        role="navigation"
+        class="inline-flex flex-wrap"
+      >
         <button
           class=${cvaChevron}
           type="button"
