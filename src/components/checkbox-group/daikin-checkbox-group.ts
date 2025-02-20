ca32df3f222842b672c76a2146c3270f47ed9337
rinjInTokyo <106000401+rinjInTokyo@users.noreply.github.com>
2025-02-20 17:45:19 +0900

    refactor: refactor by reviewer's suggestion

--- src/components/checkbox-group/daikin-checkbox-group.ts
+++ src/components/checkbox-group/daikin-checkbox-group.ts
@@ -115,7 +115,7 @@ export class DaikinCheckboxGroup extends LitElement {
         ifDefined(this.disabled as any)
       }
     >
-      <slot @slotchange=${this._handleSlotChange}> </slot>
+      <slot @slotchange=${this._handleSlotChange}></slot>
     </fieldset>`;
   }
 
