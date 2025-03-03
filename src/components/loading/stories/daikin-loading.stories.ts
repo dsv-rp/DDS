867b76fa1d30fadde535ac0123113cafc6601ecc
poetrainy <75821494+poetrainy@users.noreply.github.com>
2025-03-03 13:45:32 +0900

    feat: Implement a new loading component

---
+++ src/components/loading/stories/daikin-loading.stories.ts
@@ -0,0 +1,30 @@
+import { metadata } from "#storybook-framework";
+import { DAIKIN_LOADING_ARG_TYPES, type Story } from "./common";
+
+export default {
+  title: "Components/Loading",
+  tags: ["autodocs"],
+  argTypes: DAIKIN_LOADING_ARG_TYPES,
+  ...metadata,
+};
+
+export const Default: Story = {
+  args: {
+    size: "medium",
+    variant: "ghost",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    ...Default.args,
+    size: "small",
+  },
+};
+
+export const Fill: Story = {
+  args: {
+    ...Default.args,
+    variant: "fill",
+  },
+};
